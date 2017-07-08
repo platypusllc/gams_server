8f5c9004ae9581d94669f5a644d13bace8258841
Chris Tomaszewski <christomaszewski@gmail.com>
2017-07-08 01:13:37 +0000

    Added exports to cron job that fix automatic launching

--- cron_launch.sh
+++ cron_launch.sh
@@ -6,13 +6,22 @@
 
 #TODO: We need to check whether the controller is operating correctly even if
 #      the process is running. The controller may be hanging.
+export HOME=/home/pi
+export PROJECT_HOME=$HOME/projects
+export ACE_ROOT=$PROJECT_HOME/ace/ACE_wrappers
+export MADARA_ROOT=$PROJECT_HOME/madara
+export GAMS_ROOT=$PROJECT_HOME/gams
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ACE_ROOT/lib:$MADARA_ROOT/lib:$GAMS_ROOT/lib
+export PATH=$ACE_ROOT/bin:$MADARA_ROOT/bin:$GAMS_ROOT/bin:$PATH
+
+
 mkdir -p "$HOME/tmp"
 PIDFILE="$HOME/tmp/boat_controller.pid"
 CMD="/home/pi/gams_server/custom_controller -i 1 --madara-level 1 --gams-level 1 --broadcast 192.168.1.255:15000"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
-  echo "Already running."
+  echo "Already running." >> $HOME/tmp/boat_controller.log
   exit 99
 fi
 
